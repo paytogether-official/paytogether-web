a481cd03f2bee598d820c6282f0b9de8868b6abe
jhlov <jhlovv@gmail.com>
2025-02-13 21:28:21 +0900

    [T-63] 홈, 버튼 링크

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
+import { Create } from "./pages/Create";
+import { History } from "./pages/History";
 import { Home } from "./pages/Home";
+import { Join } from "./pages/Join";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/history" element={<History />} />
       </Routes>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { HiOutlineArrowRight } from "react-icons/hi";
+import { Link } from "react-router-dom";
 import "./Home.scss";
 
 export const Home = () => {
@@ -6,46 +7,53 @@ export const Home = () => {
     <div className="home">
       <div className="home__header flex items-center font-bold">Pay To</div>
       <div className="pt-[22px]">
-        <div className="mb-2 pt-4 pl-4 pr-2 pb-2 bg-[#2C7EFF] text-white rounded-3xl">
-          <div className="text-sm mb-0.5">
-            새로운 여행을
-            <br />
-            준비하고 있다면
-          </div>
-          <div className="font-bold mb-3">여정 생성하기</div>
-          <div className="flex justify-end">
-            <div className="arrow-icon">
-              <HiOutlineArrowRight />
+        <Link to="/create">
+          <div className="mb-2 pt-4 pl-4 pr-2 pb-2 bg-[#2C7EFF] text-white rounded-3xl">
+            <div className="text-sm mb-0.5">
+              새로운 여행을
+              <br />
+              준비하고 있다면
+            </div>
+            <div className="font-bold mb-3">여정 생성하기</div>
+            <div className="flex justify-end">
+              <div className="arrow-icon">
+                <HiOutlineArrowRight />
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
         <div className="flex gap-2">
-          <div className="flex-1 pt-4 pl-4 pr-2 pb-2 bg-[#DCEAFF] rounded-3xl">
-            <div className="text-sm mb-0.5">
-              이미 생성된
-              <br />
-              여정이 있다면
-            </div>
-            <div className="font-bold mb-3">여정 참여하기</div>
-            <div className="flex justify-end">
-              <div className="arrow-icon">
-                <HiOutlineArrowRight />
+          <Link to="/join" className="flex-1">
+            <div className="pt-4 pl-4 pr-2 pb-2 bg-[#DCEAFF] rounded-3xl">
+              <div className="text-sm mb-0.5">
+                이미 생성된
+                <br />
+                여정이 있다면
+              </div>
+              <div className="font-bold mb-3">여정 참여하기</div>
+              <div className="flex justify-end">
+                <div className="arrow-icon">
+                  <HiOutlineArrowRight />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="flex-1 pt-4 pl-4 pr-2 pb-2 bg-[#FAFAFB] rounded-3xl">
-            <div className="text-sm mb-0.5">
-              마무리된 정산을
-              <br />
-              확인하고 싶다면?
-            </div>
-            <div className="font-bold mb-3">지난 여정</div>
-            <div className="flex justify-end">
-              <div className="arrow-icon">
-                <HiOutlineArrowRight />
+          </Link>
+
+          <Link to="/history" className="flex-1">
+            <div className="pt-4 pl-4 pr-2 pb-2 bg-[#FAFAFB] rounded-3xl">
+              <div className="text-sm mb-0.5">
+                마무리된 정산을
+                <br />
+                확인하고 싶다면?
+              </div>
+              <div className="font-bold mb-3">지난 여정</div>
+              <div className="flex justify-end">
+                <div className="arrow-icon">
+                  <HiOutlineArrowRight />
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
