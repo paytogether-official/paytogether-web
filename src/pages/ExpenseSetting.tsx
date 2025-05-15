2f3c06f445d8dad02e0ecf476c1dc10d14141690
jhlov <jhlovv@gmail.com>
2025-05-15 22:49:17 +0900

    결제자 설정 바텀 시트

--- src/pages/ExpenseSetting.tsx
+++ src/pages/ExpenseSetting.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { Header } from "../components/Header";
 import { useNavigate } from "react-router";
+import { Form } from "react-bootstrap";
+import { FaCaretDown } from "react-icons/fa6";
+import { BottomSheet } from "../components/BottomSheet";
+import { FaCheckCircle } from "react-icons/fa";
 
 export const ExpenseSetting = () => {
   const navigate = useNavigate();
+
+  const [payer, setPayer] = React.useState<string>("");
+  const [showModal, setShowModal] = React.useState(false);
 
   return (
     <div className="expense-setting">
@@ -14,6 +21,40 @@ export const ExpenseSetting = () => {
           navigate("/journey");
         }}
       />
+      <div className="pt-4">
+        <Form>
+          <Form.Group className="mb-[20px]">
+            <Form.Label>결제자 설정</Form.Label>
+            <div
+              className="form-control flex justify-between items-center cursor-pointer"
+              onClick={() => setShowModal(true)}
+            >
+              <span>{payer}</span>
+              <FaCaretDown className="text-[22px]" />
+            </div>
+          </Form.Group>
+        </Form>
+      </div>
+
+      <BottomSheet
+        title="결제자 설정"
+        isOpen={showModal}
+        onClose={() => setShowModal(false)}
+      >
+        <div className="px-3 py-2 mb-2 flex justify-between items-center cursor-pointer">
+          <span className="text-[14px] font-semibold">가망이</span>
+          <FaCheckCircle className="text-[22px] text-[#2C7EFF]" />
+        </div>
+        <div className="px-3 py-2 mb-2 flex justify-between items-center cursor-pointer">
+          <span className="text-[14px] font-semibold">나망이</span>
+        </div>
+        <div className="px-3 py-2 mb-2 flex justify-between items-center cursor-pointer">
+          <span className="text-[14px] font-semibold">다망이</span>
+        </div>
+        <div className="px-3 py-2 mb-2 flex justify-between items-center cursor-pointer">
+          <span className="text-[14px] font-semibold">라망이</span>
+        </div>
+      </BottomSheet>
     </div>
   );
 };
