d69e38622446ce83a920a34540ee85826fcd12cd
jhlov <jhlovv@gmail.com>
2025-06-01 02:08:04 +0900

    여정관리 사이드메뉴

--- src/pages/Journey.tsx
+++ src/pages/Journey.tsx
@@ -1,10 +1,13 @@
+import classNames from "classnames";
 import React, { useEffect } from "react";
-import { Header } from "../components/Header";
 import { Tab, Tabs } from "react-bootstrap";
-import { JourneyAddExpense } from "./JourneyAddExpense";
-import { useParams, useLocation, useNavigate, Link } from "react-router-dom";
-import { JourneyExpenseList } from "./JourneyExpenseList";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+import { useJourney } from "store/useJourney";
+import { useJourneyList } from "store/useJourneyList";
+import { Header } from "../components/Header";
 import { SideModal } from "../components/SideModal";
+import { JourneyAddExpense } from "./JourneyAddExpense";
+import { JourneyExpenseList } from "./JourneyExpenseList";
 
 export const Journey = () => {
   const { id } = useParams<{ id: string }>();
@@ -14,27 +17,29 @@ export const Journey = () => {
   const [tab, setTab] = React.useState("ADD");
   const [showSideModal, setShowSideModal] = React.useState(false);
 
+  const { journeyList, fetchJourneyList } = useJourneyList();
+  const { journey, fetchJourney } = useJourney();
+
   useEffect(() => {
+    fetchJourneyList();
+    fetchJourney(id!);
+
     const queryParams = new URLSearchParams(location.search); // 쿼리 문자열 파싱
     const menuParam = queryParams.get("menu"); // ?example=value 읽기
     if (menuParam) {
       setTab(menuParam);
       navigate(location.pathname, { replace: true }); // URL에서 쿼리 문자열 제거
     }
-  }, []);
-
-  useEffect(() => {
-    console.log(id);
   }, [id]);
 
   return (
     <div className="journey">
       <Header
         leftType="menu"
-        title="여정명"
+        title={journey?.title}
         onClickLeft={() => setShowSideModal(true)}
       />
-      <div className="pt-4">
+      <div className="pt-3">
         <Tabs activeKey={tab} onSelect={k => setTab(k!)} className="mb-3">
           <Tab eventKey="ADD" title="지출 추가" />
           <Tab eventKey="LIST" title="목록 보기" />
@@ -48,20 +53,36 @@ export const Journey = () => {
           <div>
             <div className="text-[16px] font-bold mb-4">여정관리</div>
             <div className="flex flex-col gap-2">
-              <div className="rounded-lg bg-[#FAFAFB] px-3 py-2.5 cursor-pointer">
-                <span className="text-[14px] text-[#2C7EFF] font-semibold mr-2">
-                  여정명1
-                </span>
-                <span className="text-[12px] font-semibold rounded-lg bg-[#2C7EFF] text-[#fff] px-2 py-1">
-                  현재
-                </span>
-              </div>
-              <div className="rounded-lg bg-[#FAFAFB] px-3 py-2.5 cursor-pointer">
-                <span className="text-[14px] font-semibold">여정명2</span>
-              </div>
-              <div className="rounded-lg bg-[#FAFAFB] px-3 py-2.5 cursor-pointer">
-                <span className="text-[14px] font-semibold">여정명3</span>
-              </div>
+              {journeyList.map(item => (
+                <div
+                  key={item.journeyId}
+                  className={classNames(
+                    "rounded-lg bg-[#FAFAFB] px-3 h-[48px] flex items-center",
+                    {
+                      "cursor-pointer": item.journeyId !== journey?.journeyId
+                    }
+                  )}
+                  onClick={() => {
+                    if (item.journeyId !== journey?.journeyId) {
+                      setShowSideModal(false);
+                      navigate(`/journey/${item.journeyId}`);
+                    }
+                  }}
+                >
+                  <span
+                    className={classNames("text-[14px] font-semibold mr-2", {
+                      "text-[#2C7EFF]": item.journeyId === journey?.journeyId
+                    })}
+                  >
+                    {item.title}
+                  </span>
+                  {item.journeyId === journey?.journeyId && (
+                    <span className="text-[12px] font-semibold rounded-lg bg-[#2C7EFF] text-[#fff] px-2 py-1">
+                      현재
+                    </span>
+                  )}
+                </div>
+              ))}
             </div>
           </div>
           <div className="flex flex-col gap-2">
---
+++ src/store/useJourney.ts
@@ -0,0 +1,37 @@
+import axios from "api";
+import { Journey } from "interfaces/Journey";
+import { create } from "zustand";
+import { useCommon } from "./useCommon";
+
+interface State {
+  journey: Journey | null;
+  ///////////////////////////////////////////////////////////////////////////////////////////////
+  fetchJourney: (journeyId: string) => void;
+}
+
+export const useJourney = create<State>((set, get) => ({
+  journey: null,
+  ///////////////////////////////////////////////////////////////////////////////////////////////
+  fetchJourney: async (journeyId: string) => {
+    try {
+      const response = await axios.get<Journey>(
+        `https://api.paytogether.kr/journeys/${journeyId}`
+      );
+      if (response.status === 200) {
+        set(() => ({
+          journey: response.data
+        }));
+      } else {
+        useCommon.getState().addToast({
+          type: "error",
+          text: "여정 정보를 가져오는 데 실패했습니다."
+        });
+      }
+    } catch (error) {
+      useCommon.getState().addToast({
+        type: "error",
+        text: "여정 정보를 가져오는 데 실패했습니다."
+      });
+    }
+  }
+}));
